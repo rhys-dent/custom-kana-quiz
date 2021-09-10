d3934448bbcde0de376e53166bde418352463e8e
Rhys Dent <devRhysDent@gmail.com>
2021-09-10 11:02:33 -0600

    installed and required node dependencies. Added passport configuration to config.js

---
+++ app.js
@@ -0,0 +1,9 @@
+/**Require dependencies */
+const express = require("express");
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
+const passport = require("passport");
+const path = require("path");
+const cors = require("cors");
+
+/*End Require dependencies*/
